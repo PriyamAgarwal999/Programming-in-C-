17f5cfdfa7b17324ddb8ad13e8c12ff768184110
PriyamAgarwal999 <40921523+PriyamAgarwal999@users.noreply.github.com>
2020-12-23 02:56:14 +0530

    Add files via upload

---
+++ Alternate_of_setbase.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+cout<<17<<endl;          //decimal number
+cout<<oct<<17<<endl;    //octal number
+cout<<hex<<17<<endl;   //hexadecimal number
+    return 0;
+}
