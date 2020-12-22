69e8586a4e7c26486dc78b6d2f7b813338779ec9
PriyamAgarwal999 <40921523+PriyamAgarwal999@users.noreply.github.com>
2020-12-23 00:17:29 +0530

    Add files via upload

---
+++ setprecision(n)_using_fixed.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    double a=51.14766767676555;
+    cout<<fixed;
+    cout<<setprecision(14);
+    cout<<a;
+    return 0;
+}
