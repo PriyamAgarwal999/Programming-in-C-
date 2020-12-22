cc37ea985ea882508de3867042729a035d734bf5
PriyamAgarwal999 <40921523+PriyamAgarwal999@users.noreply.github.com>
2020-12-23 02:43:54 +0530

    Add files via upload

---
+++ setbase(b).cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+cout<<17<<endl;                   //decimal number
+cout<<setbase(8)<<17<<endl;      //octal number
+cout<<setbase(16)<<17<<endl;    //hexadecimal number
+    return 0;
+}
